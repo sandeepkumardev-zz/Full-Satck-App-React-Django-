5f8f869c0f65da82216a4979abe3695d1aa11bf6
sandy-ji-code <sandeepypb@gmail.com>
2020-03-27 19:28:52 +0530

    initial commit2

---
+++ react/build/precache-manifest.0705c1e3aeaef4ab34c5035e7b5bb3b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bcca748616516e1c9372fc63e71815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7828be296699d9106059",
+    "url": "/static/css/2.66165e39.chunk.css"
+  },
+  {
+    "revision": "78529a40e6966f2cf0b7",
+    "url": "/static/css/main.8894eec3.chunk.css"
+  },
+  {
+    "revision": "7828be296699d9106059",
+    "url": "/static/js/2.5d12ccc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d12ccc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78529a40e6966f2cf0b7",
+    "url": "/static/js/main.3402133e.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "597e208db02ddf79053d5c88c8f0f4d8",
+    "url": "/static/media/sandy.597e208d.png"
+  }
+]);
